604e7e89e67125c7305bf636fd0056e56d5a1f0e
patrickhulten <hultenpatrick@gmail.com>
2023-09-11 16:44:53 -0700

    cats deleted

--- pages/cats/[id].js
+++ pages/cats/[id].js
@@ -1,32 +1,37 @@
 import Layout from "../../components/layout";
 import { getCatIds, getCatData } from "../../lib/data";
 
-export async function getStaticProps( {params} ) {
+export async function getStaticProps({ params }) {
   const itemData = await getCatData(params.id);
   return {
     props: {
-      itemData
-    }
+      itemData,
+    },
   };
 }
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   const paths = getCatIds();
+  console.log(paths);
   return {
-    paths, 
-    fallback:false
+    paths,
+    fallback: false,
   };
 }
 
-export default function Entry( { itemData }){
-  return(
+export default function Entry({ itemData }) {
+  return (
     <Layout>
       <div className="card col-6">
         <div className="card-body">
-         <h5 className="card-title">{itemData.name}</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">{itemData.movie}</h6>
+          <h5 className="card-title">{itemData.name}</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">
+            {itemData.movie}
+          </h6>
           <p className="card-text">{itemData.breed}</p>
-          <a href="#" className="card-link">{itemData.year}</a>
+          <a href="#" className="card-link">
+            {itemData.year}
+          </a>
         </div>
       </div>
     </Layout>
